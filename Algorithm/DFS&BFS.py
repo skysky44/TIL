1c44ff1cb46992b58c7a1ead2818d5fbdb480fbc
jeonghwan <ajh5251@gmail.com>
2023-03-27 21:00:59 +0900

    Update DFS&BFS.py 재귀 사용 또는 스택/큐 사용

--- Algorithm/DFS&BFS.py
+++ Algorithm/DFS&BFS.py
@@ -1,6 +1,8 @@
 from collections import deque
 N, M, V = map(int, input().split())
 
+# dfs 재귀 사용방법
+
 
 def dfs(graph, v, visited_dfs):
     # 현재 노드 방문 처리
@@ -13,7 +15,25 @@ def dfs(graph, v, visited_dfs):
             dfs(graph, i, visited_dfs)
     # print(visited_dfs)
 
+# # dfs stack 사용방법
+# def dfs(graph, start, visited_dfs):
+#     stack = [start]
+#     # 스택이 빌 때까지 반복
+#     while stack:
+#         v = stack.pop()
+#         # 현재 노드가 아직 방문되지 않았다면,
+#         if not visited_dfs[v]:
+#             # 현재 노드를 방문 처리하고 출력합니다.
+#             visited_dfs[v] = True
+#             print(v, end=' ')
+#             # 현재 노드와 연결된 노드들 중에서,
+#             # 아직 방문되지 않은 노드들을 스택에 넣습니다.
+#             for i in reversed(graph[v]):
+#                 if not visited_dfs[i]:
+#                     stack.append(i)
 
+
+# bfs 큐 사용 방법
 def bfs(graph, start, visited_bfs):
     queue = deque([start])
     # 현재노드 방문처리
@@ -27,6 +47,15 @@ def bfs(graph, start, visited_bfs):
             if not visited_bfs[i]:
                 queue.append(i)
                 visited_bfs[i] = True
+
+# # bfs 재귀사용방법
+# def dfs(graph, v, visited_dfs):
+#     visited_dfs[v] = True
+#     print(v, end=' ')
+
+#     for i in graph[v]:
+#         if not visited_dfs[i]:
+#             dfs(graph, i, visited_dfs)
 
 
 graph = [[] for _ in range(N+1)]
