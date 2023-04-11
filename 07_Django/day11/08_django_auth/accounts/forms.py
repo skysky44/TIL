f319666abb2fcae00e7701b1b2773193913a3c56
jeonghwan <ajh5251@gmail.com>
2023-04-11 20:50:22 +0900

    Update day11

--- 07_Django/day11/08_django_auth/accounts/forms.py
+++ 07_Django/day11/08_django_auth/accounts/forms.py
@@ -19,3 +19,12 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
         fields = ('email', 'first_name', 'last_name',)
+        password = forms.CharField(
+            widget=PasswordInput(attrs={'autocomplete': 'new-password'}),
+            error_messages={
+                'required': '비밀번호를 입력해주세요.',
+                'min_length': '비밀번호는 8자 이상 입력해주세요.',
+                'max_length': '비밀번호는 32자 이하로 입력해주세요.',
+                'invalid': '비밀번호는 영문 대/소문자, 숫자, 특수문자를 포함해야 합니다.',
+            }
+        )
