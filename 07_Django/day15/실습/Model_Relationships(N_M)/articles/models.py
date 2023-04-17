b316ef40eba5950dc51fdf1994e341ad7d8a09f8
jeonghwan <ajh5251@gmail.com>
2023-04-17 18:45:37 +0900

    Add day15

---
+++ 07_Django/day15/실습/Model_Relationships(N_M)/articles/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+
+class Article(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
+    title = models.CharField(max_length=80)
+    content = models.TextField(null=False)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_comments')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.TextField(null=False)
---
+++ 07_Django/day15/실습/Model_Relationships(N_M)/articles/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Article, Comment
+from .forms import ArticleForm, CommentForm
+
+
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def detail(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    comments = article.comment_set.all()
+    comment_form = CommentForm()
+    context = {
+        'article': article,
+        'comments': comments,
+        'comment_form': comment_form,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+@login_required
+def create(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return redirect('articles:detail', article_pk=article.pk)
+
+    else:
+        form = ArticleForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'articles/create.html', context)
+
+
+@login_required
+def comment_create(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.user = request.user
+        comment.article = article
+        comment.save()
+
+    return redirect('articles:detail', article.pk)
+
+
+@login_required
+def comment_delete(request, article_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.user == comment.user:
+        comment.delete()
+
+    return redirect('articles:detail', article_pk)
+
+@login_required
+def likes(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if article.like_users.filter(pk=request.user.pk).exists():
+        article.like_users.remove(request.user)
+    else:
+        article.like_users.add(request.user)
+
+    return redirect('articles:detail', article_pk)
+
+
+def like_comments(request, article_pk, comment_pk):
+    # article = Article.objects.get(pk=article_pk)
+    comment = Comment.objects.get(pk=comment_pk)
+    if comment.like_users.filter(pk=request.user.pk).exists():
+        comment.like_users.remove(request.user)
+    else:
+        comment.like_users.add(request.user)
+
+    return redirect('articles:detail', article_pk)
